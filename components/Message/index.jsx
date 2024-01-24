0685055334a60b88dc27fdff5da429b5e373e86c
ismylsmylv <ismayilovsmyl2020@gmail.com>
2024-01-24 15:52:04 +0400

    message list added, navbar updated

--- components/Message/index.jsx
+++ components/Message/index.jsx
@@ -1,8 +1,30 @@
-import { Image, Text, View } from "react-native";
+import { Image, Text, TextComponent, TextInput, View } from "react-native";
 
 export default function Message() {
-    return <View>
-        <Image source={{ uri: "https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?cs=srgb&dl=pexels-mohamed-abdelghaffar-771742.jpg&fm=jpg" }} />
-        <Text>hi</Text>
+    return <View style={MessageStyles}>
+        <Image
+            style={{ height: 50, width: 50, marginRight: 10, borderRadius: 50 }}
+            source={{ uri: "https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?cs=srgb&dl=pexels-mohamed-abdelghaffar-771742.jpg&fm=jpg" }}
+        />
+        <View>
+            <Text style={{ fontSize: 16, fontWeight: 600, color: "black" }}>Jane Doe</Text>
+            <Text style={{ fontSize: 14, marginRight: 180 }}>How are you?</Text>
+        </View>
+        <View>
+            <Text style={{ marginBottom: 5 }}>09:23</Text>
+            <Text style={{ backgroundColor: "#20C253", textAlign: "center", height: 20, width: 20, borderRadius: 50, color: "white" }}>2</Text>
+        </View>
+
     </View>
+}
+
+const MessageStyles = {
+    padding: 10,
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "left",
+    borderBottomWidth: 1,
+    borderBottomColor: '#c9c9c9'
+
 }
--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -3,11 +3,23 @@ const DotsMenu = require("./../../assets/img/ellipsis-vertical-solid.svg")
 import DotMenu from "../../assets/img/ellipsis-vertical-solid.svg"
 export default function Navbar() {
     return <View>
-        <View style={{ display: "flex", alignItems: "center", justifyContent: "space-between", flexDirection: "column", backgroundColor: "#075E55", height: 100, width: "100%", padding: 10 }}>
-            <View style={{ display: "flex", alignItems: "center", justifyContent: "space-between", flexDirection: "row" }}>
+        <View style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            flexDirection: "column",
+            backgroundColor: "#008069",
+            height: 100,
+            width: "100%",
+            padding: 15,
+        }}>
+            <View style={{ display: "flex", alignItems: "center", justifyContent: "space-between", flexDirection: "row", alignSelf: "stretch" }}>
                 <Text style={{ color: "white", fontSize: 25, fontWeight: 500 }}>WhatsApp</Text>
-                <Image source={{ uri: "https://static-00.iconduck.com/assets.00/search-icon-2044x2048-psdrpqwp.png" }} />
-                {/* <Image source={DotMenu} style={{ height: 20 }} /> */}
+                <View style={{ display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "row" }} >
+                    <Image style={{ height: 20, width: 20, marginRight: 10 }} source={{ uri: "https://static-00.iconduck.com/assets.00/search-icon-2044x2048-psdrpqwp.png" }} />
+                    {/* <Image style={{ height: 20, width: 20 }} source={DotsMenu} /> */}
+                    {/* <Image style={{ height: 20, width: 20 }} source={{ uri: "https://cdn2.iconfinder.com/data/icons/50-material-design-round-corner-style/44/Submenu_2-512.png" }} /> */}
+                </View>
             </View>
             <View style={{ display: "flex", alignItems: "center", justifyContent: "space-between", flexDirection: "row", width: 300 }}>
                 <Pressable ><Text style={{ color: "white", fontSize: 16, fontWeight: 500 }}>Chats</Text></Pressable>
