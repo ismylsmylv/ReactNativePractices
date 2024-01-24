885ae65a776b203e1d92d6e37df60c5ee00a2d45
ismylsmylv <ismayilovsmyl2020@gmail.com>
2024-01-24 19:10:48 +0400

    components added

--- App.tsx
+++ App.tsx
@@ -7,23 +7,79 @@ import {
   ScrollView,
   Button,
   Pressable,
+  Modal,
+  TextInput,
+  StatusBar,
+  ActivityIndicator,
+  Alert,
 } from 'react-native';
 import Navbar from './components/Navbar';
 import Message from './components/Message';
+import Greet from './components/Greet';
 const ProfImg = require('./assets/img/profile.jpeg');
 
 function App(): React.JSX.Element {
+  const [modal, setmodal] = useState(false);
   const [isDisabled, setisDisabled] = useState(false);
+  const [text, settext] = useState('');
   return (
     <View>
       <Navbar />
+      <ActivityIndicator size="large" />
+      <ActivityIndicator size="small" color={'red'} animating={isDisabled} />
+      <StatusBar backgroundColor="#008069" />
+      <Text>Modal</Text>
+
+      <Button
+        title="Open modal"
+        onPress={() => {
+          // <StatusBar hidden />;
+          setmodal(!modal);
+        }}></Button>
+      <Button
+        title="Turn on"
+        onPress={() => {
+          setisDisabled(!isDisabled);
+        }}></Button>
+      <Button
+        title="Alert"
+        onPress={() => {
+          Alert.alert('Alerted', 'Hello there', [
+            {
+              text: 'General Kenobi',
+              onPress: () => Alert.alert('Good job'),
+            },
+            {
+              text: 'The What?',
+              onPress: () => Alert.alert('Pff'),
+            },
+          ]);
+        }}></Button>
       <ScrollView>
+        <Greet name={'Ken'} />
+        <Greet name={'Taylor'} />
+        <Message />
+        {/* <Message />
         <Message />
         <Message />
-        <Message />
-        <Message />
-        <Message />
+        <Message /> */}
       </ScrollView>
+      <Modal
+        visible={modal}
+        onRequestClose={() => setmodal(false)}
+        animationType="slide">
+        <View>
+          <Text>Enter text</Text>
+          {/* <TextInput onChange={(e)=>{
+            settext(e.target)
+          }}></TextInput> */}
+          <Button
+            title="close"
+            onPress={() => {
+              setmodal(!modal);
+            }}></Button>
+        </View>
+      </Modal>
     </View>
   );
 }
--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -13,10 +13,29 @@ export default function Navbar() {
             width: "100%",
             padding: 15,
         }}>
-            <View style={{ display: "flex", alignItems: "center", justifyContent: "space-between", flexDirection: "row", alignSelf: "stretch" }}>
-                <Text style={{ color: "white", fontSize: 25, fontWeight: 500 }}>WhatsApp</Text>
-                <View style={{ display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "row" }} >
-                    <Image style={{ height: 20, width: 20, marginRight: 10 }} source={{ uri: "https://static-00.iconduck.com/assets.00/search-icon-2044x2048-psdrpqwp.png" }} />
+            <View style={{
+                display: "flex",
+                alignItems: "center",
+                ustifyContent: "space-between",
+                flexDirection: "row",
+                alignSelf: "stretch"
+            }}>
+                <Text style={{
+                    color: "white",
+                    fontSize: 25,
+                    fontWeight: 500
+                }}>WhatsApp</Text>
+                <View style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    flexDirection: "row"
+                }} >
+                    <Image style={{
+                        height: 20,
+                        width: 20,
+                        marginRight: 10
+                    }} source={{ uri: "https://static-00.iconduck.com/assets.00/search-icon-2044x2048-psdrpqwp.png" }} />
                     {/* <Image style={{ height: 20, width: 20 }} source={DotsMenu} /> */}
                     {/* <Image style={{ height: 20, width: 20 }} source={{ uri: "https://cdn2.iconfinder.com/data/icons/50-material-design-round-corner-style/44/Submenu_2-512.png" }} /> */}
                 </View>
